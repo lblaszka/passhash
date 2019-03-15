facd93d761699ab9a2cc504434541d97b3640ab8
lblaszka <mlblaszka@gmail.com>
2019-03-15 01:23:26 +0100

    Hidden password on page, add getDomain() in passhash.js

--- docs/passhash.js
+++ docs/passhash.js
@@ -1,14 +1,28 @@
 //todo: add special chars to get. password
 function getPasshash( sacredPassword, urlDomain, settings )
 {
-    console.log(settings);
-
+    urlDomain = getDomain( urlDomain );
     let passhash = sha256(sacredPassword + urlDomain );
 
     if( settings != null )
         if( settings.useSpecialChars == true )
             passhash = getPassHashWithSpecialChars( passhash );
     return passhash;
+}
+
+function getDomain( url )
+{
+    //http://
+    if( 
+        ( url.substr(0,7) == "http://" )  
+        || ( url.substr(0,8) == "https://" )
+    )
+    {
+        domain = url.split("/");
+        return domain[0] + "//"+domain[2]+"/";    
+    }
+    else
+        return url;
 }
 
 function getPassHashWithSpecialChars(passhash) 
